c74042c1303d755c0b44f52d3327fbf22be97ced
ThePikachuDev <meharwansinghlehri@gmail.com>
2026-04-08 15:21:47 +0530

    Added supermaven AI because we are in 2026

--- lua/plugins/necessary.lua
+++ lua/plugins/necessary.lua
@@ -22,4 +22,33 @@ return {
 			require("mini.icons").setup({})
 		end,
 	},
+	{
+		"supermaven-inc/supermaven-nvim",
+		config = function()
+			require("supermaven-nvim").setup({
+				keymaps = {
+					accept_suggestion = "<C-f>",
+					clear_suggestion = "<C-]>",
+					accept_word = "<C-n>",
+				},
+				ignore_filetypes = { cpp = true },
+				color = {
+					suggestion_color = "#ffffff",
+					cterm = 244,
+				},
+				log_level = "info",
+				disable_inline_completion = false,
+				disable_keymaps = false,
+				condition = function()
+					return false
+				end,
+			})
+		end,
+	},
+	{
+		"mbbill/undotree",
+		config = function()
+			vim.keymap.set("n", "<leader>u", "<cmd>UndotreeToggle<CR>", { noremap = true, silent = true })
+		end,
+	},
 }
